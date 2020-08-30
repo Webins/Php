da7bd861772d42832c2b2e0875b23d90db0085e7
webins <kleivermarcano@hotmail.com>
2020-08-30 12:46:42 -0400

    changes

--- projects/Cms/includes/date.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    function get_time(){
-        date_default_timezone_set("Etc/GMT+4");
-        $time = time();
-        $time = strftime("%Y-%m-%d %H:%M:%S",$time);
-        return $time;
-    }
-?>
